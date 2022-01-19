9e9cf9a9695f0b27efab9832185df769283aebd9
Boston Rohan <bostonrohan@gmail.com>
2022-01-18 18:03:23 -0800

    refactored nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,27 +1,46 @@
 import { useState } from "react";
 import Hamburger from "hamburger-react";
 import { scroller } from "react-scroll";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./styles.css";
 function Nav({ width }) {
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
   const navbar = ["News", "About", "Games", "Players", "Quizzes"];
 
-  const handleClick = ({ item }) => {
-    open ? setOpen(false) : setOpen(open);
-    scroller.scrollTo(item, {
+  const handleClick = ({ section }) => {
+    if (location !== "/") {
+      if (section === "Players" || section === "Quizzes") {
+        navigate(section);
+      } else {
+        //Return to home, then scroll to section
+        navigate("/");
+        setTimeout(() => {
+          scroll(section);
+        }, 500);
+      }
+    } else {
+      scroll(section);
+    }
+    //Close navigation on mobile
+    setOpen(false);
+  };
+
+  const scroll = (section, delay) => {
+    return scroller.scrollTo(section, {
       smooth: true,
       delay: open ? 500 : 0,
     });
   };
-
   return (
     <div className={open ? "nav active" : "nav"}>
       {width > 600 ? (
         <ul>
-          {navbar.map((item, index) => {
+          {navbar.map((section, index) => {
             return (
-              <li key={index} onClick={() => handleClick({ item })}>
-                {item}
+              <li key={index} onClick={() => handleClick({ section })}>
+                {section}
               </li>
             );
           })}
@@ -31,10 +50,10 @@ function Nav({ width }) {
           <Hamburger toggled={open} toggle={setOpen} size={48} />
           {open && (
             <ul>
-              {navbar.map((item, index) => {
+              {navbar.map((section, index) => {
                 return (
-                  <li key={index} onClick={() => handleClick({ item })}>
-                    {item}
+                  <li key={index} onClick={() => handleClick({ section })}>
+                    {section}
                   </li>
                 );
               })}
