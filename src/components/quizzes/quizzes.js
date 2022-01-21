d95780b15a78e54d48648cef7ea8d43dda1942d2
Boston Rohan <bostonrohan@gmail.com>
2022-01-20 19:31:02 -0800

    refactored and added styles to quizzes

--- src/components/quizzes/quizzes.js
+++ src/components/quizzes/quizzes.js
@@ -1,17 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import getUser from "./utils/getUser";
-
+import "./styles.css";
 function Quizzes() {
+  const [open, setOpen] = useState(false);
   const user = localStorage.getItem("user");
   useEffect(() => {
     getUser();
   }, []);
   return (
     <section className="quizzes">
-      <section className="header">
-        <img src="./user-logos/jordan.jpg" alt="Profile Photo" />
-        <button className="user-btn">{user ? "Logout" : "Login"}</button>
-        {!user && <button className="user-btn">Login</button>}
+      <section
+        className="header"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <img
+          src={
+            user ? "./user-logos/jordan.jpg" : "./user-logos/blank-profile.png"
+          }
+          alt="Profile Photo"
+        />
+        {user && <h3>{user}</h3>}
+        {open && (
+          <>
+            <p>{user ? "Logout" : "Signup"}</p>
+            <p>{!user && "Sign in"}</p>
+          </>
+        )}
       </section>
       <div className="container">Hello World</div>
     </section>
