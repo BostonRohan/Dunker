74b39846a0f0305c385f4c7cff82348efa6ac2e7
Boston Rohan <bostonrohan@gmail.com>
2022-01-29 15:34:05 -0800

    added post request for the server to handle the quiz

--- src/components/quizzes/quiz/quiz.js
+++ src/components/quizzes/quiz/quiz.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import { useLocation } from "react-router-dom";
 import QuizData from "../quizzes.json";
 import QuizAnswerData from "../quiz-answers.json";
@@ -7,16 +8,28 @@ import "./styles.css";
 function Quiz() {
   const [question, setQuestion] = useState(0);
   const [selected, setSelected] = useState([]);
+  const [score, setScore] = useState(0);
   const location = useLocation();
   const page = location.pathname.split("/")[2];
   let possibleAnswers = QuizAnswerData[page][question];
+  console.log(score, question, page);
 
   const handleClick = (i) => {
-    if (question === 10) return;
-    //post req here to send the data to the backend to handle
-    else {
-      setSelected([...selected, i]);
-      setQuestion(question + 1);
+    setSelected([...selected, i]);
+    setQuestion(question + 1);
+
+    if (question === 9) {
+      axios
+        .post(
+          `http://localhost:5000/quiz/${page}`,
+          { [page]: selected },
+          { withCredentials: true }
+        )
+        .then((res) => {
+          console.log(res.data);
+          setScore(res.data);
+        })
+        .catch((err) => console.log(err));
     }
   };
 
@@ -40,7 +53,7 @@ function Quiz() {
           </section>
         ) : (
           <section>
-            <h2>Your Score : </h2>
+            <h2>Your Score : {score}</h2>
           </section>
         )}
       </div>
