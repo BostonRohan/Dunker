2eff5719c571be282553a154def78fc4030d9484
Boston Rohan <bostonrohan@gmail.com>
2022-02-16 08:57:08 -0800

    removed user functionality

--- src/components/quizzes/quizzes.js
+++ src/components/quizzes/quizzes.js
@@ -1,52 +1,21 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
 import QuizData from "./quizzes.json";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
 function Quizzes() {
-  const user = localStorage.getItem("user");
-  const [error, setError] = useState(undefined);
   const options = Object.keys(QuizData);
   const navigate = useNavigate();
 
-  const logout = async () => {
-    localStorage.clear();
-    window.location.reload(false);
-  };
-
-  const handleClick = (name) => {
-    if (user === null) {
-      setError("You must be signed in to complete a quiz");
-      return;
-    } else navigate(`/quiz/${name}`);
-  };
-
   return (
     <section className="quizzes">
-      <section className="login-signup">
-        {user !== null && <h4 className="user">{user}</h4>}
-        {user === null ? (
-          <>
-            <Link to="/quizzes/signup">Signup</Link>
-            <Link to="/quizzes/login">Login</Link>
-          </>
-        ) : (
-          <h4 className="logout" onClick={logout}>
-            Logout
-          </h4>
-        )}
-      </section>
       <div className="quiz-options">
         {options.map((name, i) => {
           return (
             <div
               key={i}
               className="container"
-              onClick={() => handleClick(name)}
+              onClick={() => navigate(`/quiz/${name}`)}
             >
-              {error && <h5 className="errorMsg">{error}</h5>}
               <img src="./quiz-images/nba-logo.png" alt="" />
               <p>{name}</p>
             </div>
