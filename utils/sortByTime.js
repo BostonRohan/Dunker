b9e1ffd70bd6213b2993eccc1b10758f3acf7a55
Boston Rohan <bostonrohan@gmail.com>
2022-04-01 16:52:59 -0700

    fixed sorting

--- utils/sortByTime.js
+++ utils/sortByTime.js
@@ -1,14 +1,21 @@
 const sort = (games) => {
-  //sorting by status
-  if (games.some((game) => game.status))
-    return games.sort((a, b) => +(a > b) || +(a === b) - 1);
-  //sorting by arbitrary date
-  else
-    return games.sort(function (a, b) {
-      return (
-        new Date("1970/01/01 " + a.status.split(" ")[0]) -
-        new Date("1970/01/01 " + b.status.split(" ")[0])
-      );
-    });
+  let qtrs = [];
+  let regStatus = [];
+
+  for (let i = 0; i < games.length; i++) {
+    if (games[i]["status"].includes("Qtr")) qtrs.push(games[i]);
+    else regStatus.push(games[i]);
+  }
+  regStatus = regStatus.sort(function (a, b) {
+    return (
+      new Date("1970/01/01 " + a.status.split(" ")[0]) -
+      new Date("1970/01/01 " + b.status.split(" ")[0])
+    );
+  });
+  //if there are games that have started
+  if (qtrs) {
+    qtrs = qtrs.sort((a, b) => +a.status[0] - +b.status[0]);
+    return [...qtrs, ...regStatus];
+  } else return regStatus;
 };
 export default sort;
