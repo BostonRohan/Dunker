b6cad9a7c81064e39cde20a054f565c936d4d0f4
Boston Rohan <bostonrohan@gmail.com>
2022-03-31 22:04:31 -0700

    sorting by status first

--- utils/sortByTime.js
+++ utils/sortByTime.js
@@ -1,10 +1,14 @@
 const sort = (games) => {
+  //sorting by status
+  if (games.some((game) => game.status))
+    return games.sort((a, b) => +(a > b) || +(a === b) - 1);
   //sorting by arbitrary date
-  return games.sort(function (a, b) {
-    return (
-      new Date("1970/01/01 " + a.status.split(" ")[0]) -
-      new Date("1970/01/01 " + b.status.split(" ")[0])
-    );
-  });
+  else
+    return games.sort(function (a, b) {
+      return (
+        new Date("1970/01/01 " + a.status.split(" ")[0]) -
+        new Date("1970/01/01 " + b.status.split(" ")[0])
+      );
+    });
 };
 export default sort;
