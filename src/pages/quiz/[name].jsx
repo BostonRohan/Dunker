90583c0e7eca1567b1706c7e3dc55a4c7c5c8661
Boston Rohan <bostonrohan@gmail.com>
2022-04-01 18:34:06 -0700

    fix router error

--- src/pages/quiz/[name].jsx
+++ src/pages/quiz/[name].jsx
@@ -12,7 +12,6 @@ function Quiz() {
   const [error, setError] = useState("");
   const router = useRouter();
   const page = router.query.name;
-  let quizOptions = options[page][0];
 
   const postQuiz = async () => {
     await axios
@@ -35,45 +34,47 @@ function Quiz() {
 
   return (
     <div className={styles.page}>
-      <div className={`${styles.container} container`}>
-        {error && <p className="errorMsg">{error}</p>}
-        {question < 10 ? (
-          <section>
-            {page === "player" && <p>* 2022 season</p>}
-            <h1>{quizzes[page][question]}</h1>
-            {quizOptions.map((answer, i) => {
-              return (
-                <div
-                  key={i}
-                  className={styles.answer}
-                  onClick={() => handleClick(i)}
-                >
-                  {answer}
-                </div>
-              );
-            })}
-          </section>
-        ) : (
-          score && (
+      {page && (
+        <div className={`${styles.container} container`}>
+          {error && <p className="errorMsg">{error}</p>}
+          {question < 10 ? (
             <section>
-              <h2>
-                Your Score:{" "}
-                <span className={score > 5 ? "correctMsg" : "errorMsg"}>
-                  {score + "0" + "." + "00%"}
-                </span>
-              </h2>
-              <img
-                src={
-                  score > 5
-                    ? "../quiz-images/lebron-crazy.jpg"
-                    : "../quiz-images/jordan-crying.jpg"
-                }
-                alt={score > 5 ? "LeBron James" : "Michael Jordan crying"}
-              />
+              {page === "player" && <p>* 2022 season</p>}
+              <h1>{quizzes[page][question]}</h1>
+              {options[page][question].map((answer, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={styles.answer}
+                    onClick={() => handleClick(i)}
+                  >
+                    {answer}
+                  </div>
+                );
+              })}
             </section>
-          )
-        )}
-      </div>
+          ) : (
+            score && (
+              <section>
+                <h2>
+                  Your Score:{" "}
+                  <span className={score > 5 ? "correctMsg" : "errorMsg"}>
+                    {score + "0" + "." + "00%"}
+                  </span>
+                </h2>
+                <img
+                  src={
+                    score > 5
+                      ? "../quiz-images/lebron-crazy.jpg"
+                      : "../quiz-images/jordan-crying.jpg"
+                  }
+                  alt={score > 5 ? "LeBron James" : "Michael Jordan crying"}
+                />
+              </section>
+            )
+          )}
+        </div>
+      )}
     </div>
   );
 }
